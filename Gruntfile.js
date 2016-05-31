3c23519ae8aab147e89b2db16fb3e8ea96f85306
DrummerHead <drummerhead@gmail.com>
2016-05-31 15:40:55 +0200

    Tabs to spaces

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,20 @@
 module.exports = function(grunt) {
-	var libs = "js/libs/**/*.js";
-	grunt.initConfig({
-		uglify: {
-			options: {
-				compress: false
-			},
-			js: {
-				files: {
-					"js/libs.min.js": libs
-				}
-			}
-		}
-	});
-	grunt.loadNpmTasks("grunt-contrib-uglify");
-	grunt.registerTask(
-		"default",
-		["uglify"]
-	);
+  var libs = "js/libs/**/*.js";
+  grunt.initConfig({
+    uglify: {
+      options: {
+        compress: false
+      },
+      js: {
+        files: {
+          "js/libs.min.js": libs
+        }
+      }
+    }
+  });
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.registerTask(
+    "default",
+    ["uglify"]
+  );
 };
